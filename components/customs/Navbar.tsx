ab7951b51a43adb960830e2ba57206acb37671aa
Meervv <marvine.gaudree.pro@gmail.com>
2024-08-05 11:55:34 +0200

    add navbar + presentation

---
+++ components/customs/Navbar.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import ContrastIcon from '@mui/icons-material/Contrast';
+import { toggleDarkMode } from '../../utils/darkmode';
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="navbar">
+                <ul>
+                    <li className="brand">
+                        <a href="/">Meervv</a>
+                    </li>
+                    <li>
+                        <a href="/skills">Compétences</a>
+                    </li>
+                    <li>
+                        <a href="/experience">Expériences</a>
+                    </li>
+                    <li>
+                        <a href="/education">Education</a>
+                    </li>
+                    <li>
+                        <a href="/projects">Projets</a>
+                    </li>
+                    <li>
+                        <a href="/contact">Contact</a>
+                    </li>
+                    <li className='darkmode'>
+                        <button onClick={toggleDarkMode}>
+                            <ContrastIcon />
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
---
+++ components/customs/Presentation.tsx
@@ -0,0 +1,12 @@
+export default function Presentation() {
+    return (
+        <div className="presentation">
+            <div className="presentation_content">
+                <img src="/images/moi.jpg" alt="Marvine Gaudrée" />
+                <h1>Marvine Gaudrée</h1>
+                <p>Je suis un développeur web fullstack, et je prépare un Master Expert en Technologie de l'Information, à <a className="linked" href="https://yutopia.io/">Yutopia</a></p>
+                <p>Je recherche un contrat d'alternance de 12 ou 24 mois pour la rentrée 2024, avec pour rythme 3 semaines en entreprise / 1 semaine en formation.</p>
+            </div>
+        </div>
+    )
+}
